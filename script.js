bacc4a31919bbe2eb4650f9ac2b1436014fbe1be
divyanshucse17 <divyanshu.cse17@gmail.com>
2025-08-22 22:50:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+// Get references to the select element and button
+const select = document.getElementById("colorSelect");
+const button = document.querySelector('input[type="button"]');
+
+// Add click event listener to the button
+button.addEventListener("click", () => {
+    // Get the index of the selected option
+    const selectedIndex = select.selectedIndex;
+
+    // If an option is selected, remove it
+    if (selectedIndex >= 0) {
+        select.remove(selectedIndex);
+    }
+});
